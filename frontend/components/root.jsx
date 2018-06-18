5e9d287b0a11d0c2d85e0b2bb87fc734b415d389
James Y <jameshkyoo@gmail.com>
2018-06-18 14:47:33 -0400

    set up routes for calendar

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { Switch, HashRouter } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import Modal from "./modal/modal_container";
 import NavBar from "./navbar/navbar_container";
+import HomePage from "./home_page/home_page";
+import Calendar from "./calendar/calendar";
 
 const Root = ({ store }) => (
   <Provider store={ store }>
@@ -16,6 +19,10 @@ const App = () => (
   <React.Fragment>
     <Modal/>
     <NavBar/>
+    <Switch>
+      <AuthRoute exact path="/" component={ HomePage }/>
+      <ProtectedRoute path="/my/calendar" component={ Calendar }/>
+    </Switch>
   </React.Fragment>
 );
 
